df11ba87a0178664db3d7a3aeb9267491a19056a
Andy Li <andy@onthewings.net>
2010-10-21 09:36:28 +0800

    moved hxcv to a sperated project

--- hxcv/hxcv/ds/IARGB2DImage.hx
+++
@@ -1,6 +0,0 @@
-package hxcv.ds;
-
-interface IARGB2DImage<T> implements IRGB2DImage<T> {
-	public function getA(x:Int, y:Int):T;
-	public function setA(x:Int, y:Int, val:T):Void;
-}
--- hxcv/hxcv/ds/IGray2DImage.hx
+++
@@ -1,6 +0,0 @@
-package hxcv.ds;
-
-interface IGray2DImage<T> implements I2DImage<T> {
-	public function getGray(x:Int, y:Int):T;
-	public function setGray(x:Int, y:Int, val:T):Void;
-}
